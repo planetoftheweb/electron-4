e4254e6465dc1c55bfe665fb434d8e49884011de
Ray Villalobos <ray@planetoftheweb.com>
2019-04-09 15:51:43 -0700

    finished adding browserwindow settings

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 
 function createWindows() {
-  let appWindow = new BrowserWindow();
+  let appWindow = new BrowserWindow({
+    width: 600,
+    height: 800,
+    center: true,
+    minWidth: 300
+  });
   appWindow.loadFile('./index.html');
 }
 

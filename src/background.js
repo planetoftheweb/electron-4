2ade917c1f01b68dae6bd66fe1aac9cc84be048c
Ray Villalobos <ray@planetoftheweb.com>
2019-04-15 15:04:28 -0700

    finished app

--- src/background.js
+++ src/background.js
@@ -15,7 +15,8 @@ let win
 protocol.registerStandardSchemes(['app'], { secure: true })
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 800, height: 600, icon: "icon.png" })
+  win.maximize();
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
